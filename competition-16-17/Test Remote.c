4014221aa5d18e61d6902546832c0cfcbd498659
James Fenn <TheAndroidMaster@users.noreply.github.com>
2016-12-06 18:56:58 -0500

    Update Test Remote.c

--- competition-16-17/Test Remote.c
+++ competition-16-17/Test Remote.c
@@ -19,15 +19,15 @@ int strongArmValue;
 bool klaw;
 
 task main() {
-  
+
 	SensorValue[encoder3] = 0;
 	SensorValue[encoder4] = 0;
-  
+
 	while (true) {
-    
+
 		motor[rightMotor] = (vexRT[Ch4] - vexRT[Ch3] - vexRT[Ch2] + vexRT[Ch1] + motor[rightMotor]) / 3;
 		motor[leftMotor] = (-vexRT[Ch4] - vexRT[Ch3] - vexRT[Ch2] - vexRT[Ch1] + motor[rightMotor]) / 3;
-    
+
 		if (vexRT[Btn5U] && !SensorValue(touchSensor3)) {
 			motor[armMotor3] = -getSpeed();
 			motor3Value = SensorValue(encoder3);
@@ -35,12 +35,17 @@ task main() {
 			motor[armMotor3] = getSpeed();
 			motor3Value = SensorValue(encoder3);
 		} else if (vexRT[Btn7U]) {
-			motor[armMotor3] = SensorValue(encoder3) + 215;
+			motor[armMotor3] = SensorValue(encoder3) + 175;
 			motor3Value = SensorValue(encoder3);
 		} else {
 			motor[armMotor3] = SensorValue(encoder3) - motor3Value;
 		}
-    
+		
+		if (SensorValue(touchSensor3)) {
+			motor3Value = 0;
+			SensorValue[encoder3] = 0;
+		}
+
 		if (vexRT[Btn6U]) {
 			motor[strongArm] = -getSpeed();
 			motor[strongArm2] = -getSpeed();
@@ -50,14 +55,19 @@ task main() {
 			motor[strongArm2] = getSpeed();
 			strongArmValue = 3.5 * SensorValue(encoder4);
 		} else if (vexRT[Btn7U]) {
-			motor[strongArm] = (3.5 * SensorValue(encoder4)) - (3.5 * 118);
-			motor[strongArm2] = (3.5 * SensorValue(encoder4)) - (3.5 * 118);
+			motor[strongArm] = (3.5 * SensorValue(encoder4)) - 360;
+			motor[strongArm2] = (3.5 * SensorValue(encoder4)) - 360;
 			strongArmValue = 3.5 * SensorValue(encoder4);
 		} else {
 			motor[strongArm] = (3.5 * SensorValue(encoder4)) - strongArmValue;
 			motor[strongArm2] = (3.5 * SensorValue(encoder4)) - strongArmValue;
 		}
-    
+		
+		if (SensorValue(touchSensor4)) {
+			strongArmValue = 0;
+			SensorValue[encoder4] = 0;
+		}
+
 		if (vexRT[Btn7R]) {
 			motor[klaw1] = 127;
 			motor[klaw2] = 127;
@@ -75,17 +85,17 @@ task main() {
 				motor[klaw2] = -5;
 			}
 		}
-    
+
 		displayLCDNumber(0, 0, SensorValue(encoder3));
-		displayLCDNumber(0, 6, SensorValue(encoder4));
-    
+		displayLCDNumber(0, 6, SensorValue(encoder4) * 3.5);
+
 		if (vexRT[Btn7D]) {
 			while ((!SensorValue(touchSensor3) || !SensorValue(touchSensor4)) && vexRT[Btn7D]) {
 				if (!SensorValue(touchSensor3)) {
 					motor[armMotor3] = -70;
 					motor3Value = SensorValue(encoder3);
 				} else motor[armMotor3] = 0;
-        
+
 				if (!SensorValue(touchSensor4)) {
 					motor[strongArm] = 70;
 					motor[strongArm2] = 70;
@@ -95,11 +105,13 @@ task main() {
 					motor[strongArm2] = 0;
 				}
 			}
-      
+
 			SensorValue[encoder3] = 0;
 			SensorValue[encoder4] = 0;
+			motor3Value = 0;
+			strongArmValue = 0;
 		}
-	} 
+	}
 }
 
 float getSpeed() {
